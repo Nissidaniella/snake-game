9d168c84a02cda5b4ee0af14ce64b332d96cb35c
Beniah Opong Diallo <136281757+Beniah1@users.noreply.github.com>
2023-09-28 07:07:51 -1200

    Improve snake game logic

--- script.js
+++ script.js
@@ -75,5 +75,17 @@ const initGame =() => {
     if (snakeX <= 0 || snakeX > 30 || snakeY < 0 || snakeY > 30) {
         return gameOver = true;
     }
-
+for (let i = 0 ); i < snakeBody.length; i++) {
+    // Adding a div for each part of the snakes body 
+    html += '<div class="head" style= "grid-area: ${snakeBody[i][1]}/${snakeBody[i][0]}"></div>';
+    // Checking if the snake hit the body, if so set gameOver to true
+    if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody [0][0] === snakeBody[i][0]) {
+         gameOver = true;
+    }
 }
+playBoard.innerHTML = html;
+}
+
+updateFoodPosition();
+setIntervalId = setInterval(initGame, 100);
+document.addEventListener("keydown", changeDirection);
